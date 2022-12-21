ee5e62e716b4a6bfc33965252c1e6e8ae922195b
Jacob Bolden <jlbolden.pro@gmail.com>
2022-12-21 09:16:40 -0600

    Added state for long messages

--- src/components/prompt/description.tsx
+++ src/components/prompt/description.tsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+
 export default function Description({ description }: { description: string | null }) {
+ const [view, setView] = useState(false);
  if (!description) return <></>;
- return <p className="inline">{description}</p>;
+ return (
+  <>
+   <p className="inline">{view ? description : description.slice(0, 128)}</p>
+   {description.length > 128 && (
+    <button
+     className="inline font-bold text-secondary hover:opacity-100 focus:opacity-100 dark:text-primary opacity-50"
+     onClick={() => setView((p) => !p)}
+    >
+     &nbsp;
+     {view ? "less" : "more"}..
+    </button>
+   )}
+  </>
+ );
 }
