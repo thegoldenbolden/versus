2433904f8b8ee5dde7f39da5004df51b9b1662f3
Jacob Bolden <jlbolden.pro@gmail.com>
2022-12-21 09:21:50 -0600

    Changed style

--- src/layouts/auth.tsx
+++ src/layouts/auth.tsx
@@ -4,7 +4,7 @@ import { bebas } from "@lib/fonts";
 
 const AuthLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
  return (
-  <div className="center-page">
+  <div className="grid h-screen w-screen place-content-center">
    <main className="flex flex-col w-full sm:w-[480px] items-center justify-center gap-2 p-4 sm:gap-4">
     <h1 className={`${bebas.className} text-4xl sm:text-6xl text-center`}>VersusZero</h1>
     <div className="auth">{children}</div>
