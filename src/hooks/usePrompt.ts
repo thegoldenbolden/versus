f255e15458bec4f5b6b4d3295b1465c0031e7747
Jacob Bolden <jlbolden.pro@gmail.com>
2022-12-21 09:20:55 -0600

    Added comments

--- src/hooks/usePrompt.ts
+++ src/hooks/usePrompt.ts
@@ -1,9 +1,8 @@
 import { formatQuery } from "@lib/format";
 import useSWRInfinite from "swr/infinite";
-import { MAX_PROMPTS_PER } from "@lib/constants";
 
 // prettier-ignore
-const fallbackData = { prompts: [] as Versus.Prompt[], pagination: { count: 0, back: null, next: null }};
+const fallbackData = { prompts: [] as Versus.Prompt[], pagination: { back: null, next: null }};
 
 type Args = {
  query?: Omit<Versus.GetPromptArgs, "take" | "pid"> & { pid?: string | null };
@@ -13,12 +12,19 @@ type Args = {
 const getKey = (page: number, previous: any, args: Args) => {
  if (args.suspend) return null;
  const query = { ...args.query };
- if (previous?.pagination?.next) query.cursor = previous.pagination.next;
+
+ // If the previous page shows more data can be fetched set the cursor
+ if (previous?.pagination?.next) {
+  query.cursor = previous.pagination.next;
+ }
+
  return `/api/prompts?${formatQuery(query)}`;
 };
 
 const usePrompts = (args: Args) => {
+ // Prevent fetching until a condition is met.
  args.suspend ??= false;
+
  // prettier-ignore
  const { data, error, size, mutate, setSize, isValidating } = useSWRInfinite((i, p) => getKey(i, p, args));
  const pages = data ? [].concat(...data) : ([] as typeof fallbackData[]);
@@ -26,10 +32,11 @@ const usePrompts = (args: Args) => {
  const loadingInitial = !data && !error;
  // prettier-ignore
  const loadingMore = loadingInitial || (size > 0 && data && typeof data[size - 1] === "undefined");
- const isEmpty = data?.[0]?.prompts?.length === 0;
+ const isEmpty = data?.[0]?.prompts?.length === 0 ?? true;
 
  // prettier-ignore
- const reachingEnd = isEmpty || (data && data[data.length - 1]?.prompts?.length < MAX_PROMPTS_PER);
+ // If next equals null then no more data can be fetched
+ const reachingEnd = isEmpty || (data && !data[data.length - 1]?.pagination.next);
  const isRefreshing = isValidating && data && data.length === size;
 
  // prettier-ignore
