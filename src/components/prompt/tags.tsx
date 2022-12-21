b22af1c540ac4ec69254790a66d7138ec51dc7c2
Jacob Bolden <jlbolden.pro@gmail.com>
2022-12-21 09:22:26 -0600

    Lookup tag names

--- src/components/prompt/tags.tsx
+++ src/components/prompt/tags.tsx
@@ -1,17 +1,20 @@
+import { TAGS } from "@lib/constants";
 import Link from "next/link";
 
-export default function Tags({ tags }: { tags: Versus.Tag[] }) {
+export default function Tags({ tags }: { tags: number[] }) {
  if (!tags || tags.length === 0) return null;
  return (
   <div className="flex flex-wrap gap-2 text-xs font-bold">
    {tags.map((tag) => {
+    const t = TAGS.find((t) => t.id === tag);
+    if (!t) return null;
     return (
      <Link
-      href={`/explore?tag=${tag.id}`}
-      key={tag.name}
+      href={`/explore?tags=${t.id}`}
+      key={t.id}
       className="opacity-75 hover:underline focus:underline hover:opacity-100 focus:opacity-100"
      >
-      #{tag.name}
+      #{t.name}
      </Link>
     );
    })}
