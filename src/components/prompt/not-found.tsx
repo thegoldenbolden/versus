9f9fc8b096bcd21a1e8b6380399e32efad543513
Jacob Bolden <jlbolden.pro@gmail.com>
2022-12-21 09:18:31 -0600

    Changed text

--- src/components/prompt/not-found.tsx
+++ src/components/prompt/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function notFound() {
  return (
   <div className="flex flex-col gap-2 justify-center w-full h-full items-center">
-   <span>Versus Not Found</span>
+   <span>No Versus Found</span>
    <span>You can always create one!</span>
    <Link href="/create" className="btn-primary">
     Create
