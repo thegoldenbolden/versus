a25cfaa42fde4f679aed503e52fd6670934d24e2
Jacob Bolden <jlbolden.pro@gmail.com>
2022-12-21 09:29:02 -0600

    Fixes

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,66 +1,92 @@
 import { GetServerSideProps } from "next";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 import Searchbar from "@components/input/searchbar";
 import { formatNumber } from "@lib/format";
 import Loading from "@components/loading";
 import RootLayout from "@layouts/root";
 import usePrompt from "@hooks/usePrompt";
-import Description from "@components/prompt/description";
 import Tags from "@components/prompt/tags";
+import Spinner from "@components/spinner";
 
-// TODO: possibly refresh page every search
-// TODO: move prompt total count to own route
+// TODO: add comments
 
 const Explore = ({ query: q }: { query: Versus.GetPromptArgs }) => {
  const [query, setQuery] = useState<Versus.GetPromptArgs>(q);
  const infinite = usePrompt({ query });
+ const router = useRouter();
 
- const updateParams = (key: keyof Versus.GetPromptArgs, value: any) => {
-  setQuery({ ...query, ...{ [key]: value } });
- };
+ useEffect(() => {
+  const update: any = {};
+  const { query: rq } = router;
+  if (rq["title"]) update.q = rq.title as string;
+  if (rq["tags"]) update.tags = rq.tags as string;
+  setQuery((p) => ({ ...p, ...update }));
+ }, [router, query]);
 
  return (
-  <div className="p-4">
+  <>
    <Searchbar />
-   <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+   <div className="grid gap-4 px-2 py-4 place-content-center grid-cols-explore">
     {infinite.loadingInitial && <Loading />}
     {infinite.isEmpty && <span>No Versus Were Found</span>}
-    {infinite.pages.map((page) => {
-     if (!page) <></>;
-     return page.prompts.map((prompt) => {
-      return (
-       <div
-        className="flex flex-col justify-between w-full max-w-[300px] h-full p-4 rounded-md bg-light aspect-video shadow-default"
-        key={prompt.number}
-       >
-        <div className="flex flex-col gap-[0.25rem]">
-         <Link
-          href={`/${prompt.number}`}
-          className="font-display text-2xl truncate max-w-[250px] hover:underline focus:underline"
-         >
-          {prompt.title}
-         </Link>
-         <span className="block text-xs font-bold">by {prompt.author.name}</span>
-         <div className="flex flex-wrap gap-2 text-xs text-secondary dark:text-primary">
-          <span>{formatNumber(prompt.likes)} likes</span>•
+    {!infinite.loadingInitial &&
+     infinite.pages.map((page) => {
+      if (!page) <></>;
+      return page.prompts.map((prompt) => {
+       return (
+        <div
+         className="flex flex-col gap-2 p-2 rounded-sm btn-hover bg-theme color-theme shadow-default"
+         key={prompt.number}
+        >
+         <div className="flex flex-col gap-[0.25rem]">
           <Link
-           href={`/${prompt.number}/comments`}
-           className="hover:underline focus:underline"
+           href={`/p/${prompt.number}`}
+           className="text-2xl truncate font-display hover:underline focus:underline"
           >
-           {formatNumber(prompt.comments)} comments
+           {prompt.title}
           </Link>
+          <span className="block text-sm font-bold">by {prompt.author.name}</span>
+          <div className="flex flex-wrap gap-2 text-xs text-secondary dark:text-primary">
+           <span>{formatNumber(prompt.likes)} likes</span>
+           {/* <Link
+            href={`/p/${prompt.number}/comments`}
+            className="hover:underline focus:underline"
+           >
+            {formatNumber(prompt.comments)} comments
+           </Link> */}
+          </div>
          </div>
+         <p className="truncate">
+          {!prompt.description
+           ? "No description available"
+           : prompt.description.slice(0, 36)}
+         </p>
+         <Tags tags={prompt.tags} />
         </div>
-        <Description description={prompt.description} />
-        <Tags tags={prompt.tags} />
-       </div>
-      );
-     });
-    })}
+       );
+      });
+     })}
+    {!infinite.reachingEnd && !infinite.loadingInitial && (
+     <button
+      className="flex items-center justify-center w-full gap-2 px-4 py-2 rounded-sm btn-hover bg-theme color-theme shadow-default"
+      disabled={infinite.isRefreshing || infinite.loadingMore}
+      onClick={() => infinite.setSize((p) => p + 1)}
+     >
+      {infinite.isRefreshing || infinite.loadingMore ? (
+       <>
+        <Spinner />
+        <span>Loading</span>
+       </>
+      ) : (
+       <>Load More</>
+      )}
+     </button>
+    )}
    </div>
-  </div>
+  </>
  );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,88 @@
 import RootLayout from "@layouts/root";
 import Searchbar from "@components/input/searchbar";
 import ErrorBoundary from "@components/error";
-import PromptFeed from "@components/prompt/feed-prompts";
+import Spinner from "@components/spinner";
+import usePrompts from "@hooks/usePrompt";
+import FallbackPrompt from "@components/prompt/fallback";
+import { Fragment } from "react";
+import Prompt from "@components/prompt";
+import Link from "next/link";
+
+function LoadingInitial() {
+ return (
+  <>
+   {Array.from({ length: 5 }).map((x, i) => {
+    return (
+     <Fragment key={i}>
+      <FallbackPrompt />
+     </Fragment>
+    );
+   })}
+  </>
+ );
+}
+
+function isEmpty() {
+ return (
+  <div className="flex flex-col items-center self-center justify-center w-screen h-full gap-4 mt-5 mb-auto">
+   <span className="text-2xl">Currently, there are no versus D:</span>
+   <Link href="/create" className="text-3xl font-display">
+    Create A Versus
+   </Link>
+  </div>
+ );
+}
 
 const Page = () => {
+ const infinite = usePrompts({});
+
+ const Component =
+  infinite.loadingInitial || infinite.isRefreshing
+   ? LoadingInitial
+   : infinite.isEmpty
+   ? isEmpty
+   : null;
+
  return (
-  <div className="flex flex-col items-center w-full gap-4 ml-auto mr-auto sm:w-max md:py-2">
-   <Searchbar />
+  <div className="flex flex-col items-center w-full min-h-screen gap-4 pb-2">
    <ErrorBoundary>
-    <div className="grid grid-cols-[minmax(100%,480px)] xl:grid-cols-[480px_480px] md:gap-4">
-     <PromptFeed />
+    <Searchbar />
+    <div className="flex flex-col w-[min(100%,480px)] gap-4">
+     {Component ? (
+      <Component />
+     ) : (
+      <>
+       {infinite.pages.map((page) => {
+        return page.prompts.map((prompt) => {
+         return (
+          <Fragment key={prompt.number}>
+           <Prompt
+            data={prompt}
+            // mutate={infinite.mutate}
+            isRefreshing={infinite.isRefreshing}
+           />
+          </Fragment>
+         );
+        });
+       })}
+       {!infinite.reachingEnd && (
+        <button
+         className="flex items-center justify-center w-full gap-2 px-4 py-2 rounded-sm btn-hover bg-theme color-theme shadow-default"
+         disabled={infinite.isRefreshing || infinite.loadingMore}
+         onClick={() => infinite.setSize((p) => p + 1)}
+        >
+         {infinite.isRefreshing || infinite.loadingMore ? (
+          <>
+           <Spinner />
+           <span>Loading</span>
+          </>
+         ) : (
+          <>Load More</>
+         )}
+        </button>
+       )}
+      </>
+     )}
     </div>
    </ErrorBoundary>
   </div>
