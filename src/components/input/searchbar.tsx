3798e2019f8de839a304e0366cfb8b1ee61ca7d6
Jacob Bolden <jlbolden.pro@gmail.com>
2022-12-21 09:16:16 -0600

    Added useRouter

--- src/components/input/searchbar.tsx
+++ src/components/input/searchbar.tsx
@@ -1,28 +1,33 @@
 "use client";
 import { useState, FormEventHandler } from "react";
-import Router from "next/router";
+import { useRouter } from "next/router";
 
 import { ISearch } from "@components/icons";
 import { TextInput } from "@components/input/index";
 
-export default function Searchbar({ className }: { className?: string }) {
+type SearchbarProps = { className?: string };
+
+export default function Searchbar(props: SearchbarProps) {
  const [value, setValue] = useState("");
+ const router = useRouter();
 
  const defaultClass =
-  "sticky top-0 z-10 bg-light/75 dark:bg-dark/75 flex items-center w-full gap-4 px-4 md:top-4 color-theme shadow-default font-display focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-blue-900";
+  "btn-hover shadow-default transition-colors sticky top-0 z-10 bg-light/75 dark:bg-dark/75 flex items-center w-full gap-4 px-4 color-theme shadow-defefault";
 
  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
   e.preventDefault();
 
   if (value.length > 0) {
-   await Router.push(`/explore?title=${value.replaceAll(" ", "+")}`);
-   return;
+   router.push({
+    pathname: "/explore",
+    query: { q: value },
+   });
   }
  };
 
  return (
-  <form onSubmit={handleSubmit} className={`${defaultClass} ${className}`}>
-   <ISearch className="icon" />
+  <form onSubmit={handleSubmit} className={`${defaultClass} ${props.className}`}>
+   <ISearch className="w-6 h-6" />
    <TextInput
     id="searchbar"
     className="py-4 bg-transparent text-md grow"
