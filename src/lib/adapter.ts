f564bccbe66396a31c9c8ab774ba21ae4a2c125c
Jacob Bolden <jlbolden.pro@gmail.com>
2023-01-23 04:26:21 -0600

    Fixed create session missing userId

--- src/lib/adapter.ts
+++ src/lib/adapter.ts
@@ -1,51 +1,64 @@
+// @ts-nocheck
 import type { Adapter } from "next-auth/adapters";
 import nanoid from "@lib/nanoid";
 import p from "@lib/prisma";
 
-export default function MyAdapter(): Adapter {
+export default function PrismaAdapter(): Adapter {
  return {
-  createUser: async (data) => {
-   return await p.user.create({ data: { ...data, id: nanoid() } }).catch((e) => e);
+  createUser: (data) => p.user.create({ data, id: nanoid() }),
+  getUser: (id) => p.user.findUnique({ where: { id } }),
+  getUserByEmail: (email) => p.user.findUnique({ where: { email } }),
+  async getUserByAccount(provider_providerAccountId) {
+   var _a;
+   const account = await p.account.findUnique({
+    where: { provider_providerAccountId },
+    select: { user: true },
+   });
+   return (_a = account === null || account === void 0 ? void 0 : account.user) !==
+    null && _a !== void 0
+    ? _a
+    : null;
   },
-  getUser: async (id) => await p.user.findUnique({ where: { id } }).catch((e) => e),
-  getUserByEmail: async (email) =>
-   await p.user.findUnique({ where: { email } }).catch((e) => e),
-  getUserByAccount: async (provider_providerAccountId) =>
-   await p.account
-    .findUnique({ where: { provider_providerAccountId }, select: { user: true } })
-    .catch((e) => e),
-  updateUser: async ({ id, ...data }) =>
-   await p.user.update({ where: { id }, data }).catch((e) => e),
-  deleteUser: async (id) => await p.user.delete({ where: { id } }).catch((e) => e),
-  linkAccount: async (data) =>
-   await p.account
-    .create({
-     data: { ...data, access_token: undefined, refresh_token: undefined },
-    })
-    .catch((e) => e),
-  unlinkAccount: async (provider_providerAccountId) =>
-   await p.account
-    .delete({
-     where: { provider_providerAccountId },
-    })
-    .catch((e) => e),
-  getSessionAndUser: async (sessionToken) => {
-   const userAndSession = await p.session
-    .findUnique({
-     where: { sessionToken },
-     include: { user: true },
-    })
-    .catch((e) => e);
+  updateUser: ({ id, ...data }) => p.user.update({ where: { id }, data }),
+  deleteUser: (id) => p.user.delete({ where: { id } }),
+  linkAccount: (data) => p.account.create({ data }),
+  unlinkAccount: (provider_providerAccountId) =>
+   p.account.delete({
+    where: { provider_providerAccountId },
+   }),
+  async getSessionAndUser(sessionToken) {
+   const userAndSession = await p.session.findUnique({
+    where: { sessionToken },
+    include: { user: true },
+   });
    if (!userAndSession) return null;
    const { user, ...session } = userAndSession;
    return { user, session };
   },
-  createSession: async (data) => await p.session.create({ data }).catch((e) => e),
-  updateSession: async (data) =>
-   await p.session
-    .update({ where: { sessionToken: data.sessionToken }, data })
-    .catch((e) => e),
-  deleteSession: async (sessionToken) =>
-   await p.session.delete({ where: { sessionToken } }).catch((e) => e),
+  createSession: (data) => p.session.create({ data }),
+  updateSession: (data) =>
+   p.session.update({ where: { sessionToken: data.sessionToken }, data }),
+  deleteSession: (sessionToken) => p.session.delete({ where: { sessionToken } }),
+  async createVerificationToken(data) {
+   const verificationToken = await p.verificationToken.create({ data });
+   // @ts-expect-errors // MongoDB needs an ID, but we don't
+   if (verificationToken.id) delete verificationToken.id;
+   return verificationToken;
+  },
+  async useVerificationToken(identifier_token) {
+   try {
+    const verificationToken = await p.verificationToken.delete({
+     where: { identifier_token },
+    });
+    // @ts-expect-errors // MongoDB needs an ID, but we don't
+    if (verificationToken.id) delete verificationToken.id;
+    return verificationToken;
+   } catch (error) {
+    // If token already used/deleted, just return null
+    // https://www.prisma.io/docs/reference/api-reference/error-reference#p2025
+    if (error.code === "P2025") return null;
+    throw error;
+   }
+  },
  };
 }
