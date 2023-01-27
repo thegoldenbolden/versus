1697502bef0e21dea6b38f67541985ab86079401
Jacob Bolden <jlbolden.pro@gmail.com>
2023-01-26 18:31:59 -0600

    Fixed reusability

--- src/components/versus/feed.tsx
+++ src/components/versus/feed.tsx
@@ -1,8 +1,9 @@
 import type { PropsWithChildren, FC } from "react";
+import type { InfiniteData, UseInfiniteQueryResult } from "@tanstack/react-query";
+import type { Mutate } from "types/mutate";
+
 import { Fragment, useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
-import { useFeedMutation } from "@hooks/use-feed-mutation";
-import useFeed from "@hooks/use-feed";
 import CONFIG from "@lib/versus/config";
 import Versus from "../versus";
 import FallbackVersus from "../versus/fallback";
@@ -11,69 +12,75 @@ import Spinner from "../loading/spinner";
 import CustomError from "../ui/error";
 import Header from "../feed/header";
 
-const VersusFeed: VersusFeed = ({ query }) => {
+const VersusFeed: VersusFeed = ({ children, status, data, ...props }) => {
  const [tab, setTab] = useState<number>(0);
- const { inView, ref } = useInView();
+ const displaySingle = tab === 1;
+ let Component = null;
 
- const { hasNextPage, fetchNextPage, ...feed } = useFeed(query);
- const mutation = useFeedMutation(query);
-
- // Fetch more data when the load more button is in view.
- useEffect(() => {
-  inView && hasNextPage && fetchNextPage();
- }, [inView, hasNextPage, fetchNextPage]);
-
- const loadingOrError =
-  feed.status === "loading" ? (
-   <LoadingInitial />
-  ) : feed.status === "error" ? (
-   <CustomError />
-  ) : null;
+ if (status === "loading") {
+  Component = <LoadingInitial />;
+ } else if (status === "error") {
+  Component = <CustomError />;
+ } else if (!data?.pages[0]) {
+  Component = <VersusNotFound />;
+ }
 
  return (
   <>
-   {loadingOrError ? (
-    loadingOrError
-   ) : (
-    <>
-     {!feed.data?.pages[0] ? (
-      <VersusNotFound />
-     ) : (
-      <>
-       <Header tab={tab} setTab={setTab} />
-       {feed.data?.pages.map((page) => {
+   <div className="flex flex-col w-full divide-y divide-smoky-black-translucent dark:divide-lotion-translucent">
+    <Header tab={tab} setTab={setTab}>
+     {children}
+    </Header>
+    {Component
+     ? Component
+     : data?.pages.map((page) => {
         return page?.items.map((versus, i) => (
          <Fragment key={versus.id}>
-          <Versus data={{ versus, displaySingle: tab == 1 }} mutation={mutation} />
+          <Versus data={{ versus, displaySingle }} mutation={props.mutation} />
          </Fragment>
         ));
        })}
-       <button
-        ref={ref}
-        onClick={() => hasNextPage && fetchNextPage()}
-        className="flex items-center justify-center w-full gap-2 p-2 mb-16 text-lg text-center bg-transparent border-2 border-transparent border-solid text-smoky-black dark:text-lotion border-t-smoky-black-translucent dark:border-t-lotion-translucent font-display sm:mb-0"
-        disabled={!hasNextPage || feed.isFetchingNextPage}
-       >
-        {feed.isFetchingNextPage ? (
-         <>
-          <Spinner />
-          <>Loading more...</>
-         </>
-        ) : hasNextPage ? (
-         <>Load more</>
-        ) : (
-         <>You reached the end!</>
-        )}
-       </button>
-      </>
-     )}
-    </>
+   </div>
+   {data?.pages[0] && (
+    <LoadMore
+     hasNextPage={props.hasNextPage}
+     isFetchingNextPage={props.isFetchingNextPage}
+     fetchNextPage={props.fetchNextPage}
+    />
    )}
   </>
  );
 };
 
 export default VersusFeed;
+
+function LoadMore({ hasNextPage, fetchNextPage, isFetchingNextPage }: Infinite) {
+ const { inView, ref } = useInView();
+
+ useEffect(() => {
+  inView && hasNextPage && fetchNextPage();
+ }, [inView, hasNextPage, fetchNextPage]);
+
+ return (
+  <button
+   ref={ref}
+   onClick={() => hasNextPage && fetchNextPage()}
+   className="flex items-center justify-center w-full gap-2 p-2 mb-16 text-lg text-center bg-transparent border-2 border-transparent border-solid text-smoky-black dark:text-lotion border-t-smoky-black-translucent dark:border-t-lotion-translucent font-display sm:mb-0"
+   disabled={!hasNextPage || isFetchingNextPage}
+  >
+   {isFetchingNextPage ? (
+    <>
+     <Spinner />
+     <>Loading more...</>
+    </>
+   ) : hasNextPage ? (
+    <>Load more</>
+   ) : (
+    <>You reached the end!</>
+   )}
+  </button>
+ );
+}
 
 function LoadingInitial() {
  return (
@@ -89,5 +96,17 @@ function LoadingInitial() {
  );
 }
 
-type VersusFeed = FC<PropsWithChildren & { route?: string; query?: Query }>;
-type Query = any;
+type VersusFeed = FC<
+ PropsWithChildren &
+  Infinite & {
+   data: InfiniteData<Versus.ResponsePagination<Versus.Versus> | null> | undefined;
+   status: string;
+   mutation: Mutate;
+  }
+>;
+
+type Infinite = {
+ hasNextPage: boolean | undefined;
+ isFetchingNextPage: boolean | undefined;
+ fetchNextPage: UseInfiniteQueryResult["fetchNextPage"];
+};
