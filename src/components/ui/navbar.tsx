6a690a9e53a4568781bb5d59dd6b8106db821791
Jacob Bolden <jlbolden.pro@gmail.com>
2023-01-26 18:29:31 -0600

    Added explore and profile routes

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 
 // prettier-ignore
-import { IUserLine, IHomeLine, IHomeFill,  IUserFill, ILoginLine, ILogoutLine } from "./icons";
+import { IUserLine, IHomeLine, IHomeFill,  IUserFill, ILoginLine, ILogoutLine, ISearchFill, ISearchLine } from "./icons";
 import Create from "../buttons/create";
 import Avatar from "../user/avatar";
 
@@ -12,8 +12,10 @@ export default function Header() {
  const isActive = (path: string) => router.pathname === path;
 
  const activeHome = isActive("/");
- const activeProfile = isActive("/account");
+ const activeProfile = isActive("/[username]");
+ const activeExplore = isActive("/explore");
  const Home = activeHome ? IHomeFill : IHomeLine;
+ const Explore = activeExplore ? ISearchFill : ISearchLine;
 
  return (
   <header className="fixed bottom-0 z-50 w-full bg-white shadow-md sm:px-2 dark:bg-black shadow-black sm:shadow-none sm:sticky sm:top-0 sm:bg-transparent sm:dark:bg-transparent sm:h-min sm:flex sm:flex-col sm:items-center md:items-start sm:w-auto">
@@ -28,13 +30,23 @@ export default function Header() {
       activeHome ? "font-bold" : ""
      } text-inherit py-3 w-full flex items-center justify-center border-none rounded-none px-2 sm:py-2 bg-transparent transition-colors hover:bg-smoky-black-translucent hover:backdrop-blur-sm focus:bg-smoky-black-translucent hover:dark:bg-lotion-translucent focus:dark:bg-lotion-translucent sm:px-2 sm:rounded-full xl:py-2 xl:justify-start xl:rounded-md xl:gap-6`}
     >
-     <Home className="w-7 h-7" />
+     <Home aria-label="home icon" className="w-7 h-7" />
      <span className="hidden xl:inline-flex">Home</span>
+    </Link>
+    <Link
+     title="Explore"
+     href="/explore"
+     className={`${
+      activeExplore ? "font-bold" : ""
+     } text-inherit py-3 w-full flex items-center justify-center border-none rounded-none px-2 sm:py-2 bg-transparent transition-colors hover:bg-smoky-black-translucent hover:backdrop-blur-sm focus:bg-smoky-black-translucent hover:dark:bg-lotion-translucent focus:dark:bg-lotion-translucent sm:px-2 sm:rounded-full xl:py-2 xl:justify-start xl:rounded-md xl:gap-6`}
+    >
+     <Explore aria-label="search icon" className="w-7 h-7" />
+     <span className="hidden xl:inline-flex">Explore</span>
     </Link>
     <UserRoutes activeProfile={activeProfile} />
     <Create
      overrideClass
-     className="flex items-center justify-center order-3 w-full px-2 py-3 bg-transparent border-2 border-solid border-none rounded-none sm:py-2 font-display border-primary dark:border-secondary hover:bg-smoky-black-translucent hover:backdrop-blur-sm focus:bg-smoky-black-translucent hover:dark:bg-lotion-translucent focus:dark:bg-lotion-translucent sm:rounded-full sm:order-last xl:rounded-md xl:bg-primary xl:dark:bg-secondary xl:hover:bg-transparent xl:focus:bg-transparent xl:active:bg-transparent xl:border-solid xl:border-2 xl:hover:border-primary xl:focus:border-primary xl:active:border-primary xl:hover:dark:border-secondary xl:focus:dark:border-secondary xl:hover:text-primary xl:focus:text-primary xl:dark:hover:text-secondary xl:dark:focus:text-secondary xl:text-lotion"
+     className="flex items-center justify-center order-3 w-full px-2 py-3 bg-transparent border-2 border-transparent border-solid rounded-none :hover:bg-smoky-black-translucent dark:hover:bg-lotion-translucent sm:py-2 font-display sm:rounded-full sm:order-last xl:rounded-md xl:bg-smoky-black xl:text-lotion xl:dark:bg-lotion xl:dark:text-smoky-black xl:hover:bg-smoky-black/75 xl:focus:bg-smoky-black/75 xl:dark:hover:bg-lotion/90 xl:dark:focus:bg-lotion/90"
     />
    </nav>
   </header>
@@ -51,7 +63,7 @@ function UserRoutes({ activeProfile }: { activeProfile: boolean }) {
     href="/logout"
     className="flex items-center justify-center order-last w-full px-2 py-3 transition-colors bg-transparent border-none rounded-none sm:py-2 text-inherit hover:bg-smoky-black-translucent hover:backdrop-blur-sm focus:bg-smoky-black-translucent hover:dark:bg-lotion-translucent focus:dark:bg-lotion-translucent sm:p-2 sm:rounded-full xl:py-2 xl:justify-start xl:rounded-md xl:gap-6"
    >
-    <ILogoutLine className="w-7 h-7" />
+    <ILogoutLine aria-label="logout icon" className="w-7 h-7" />
     <span className="hidden xl:inline-flex"> Logout</span>
    </Link>
   ) : (
@@ -60,7 +72,7 @@ function UserRoutes({ activeProfile }: { activeProfile: boolean }) {
     href="/login"
     className="flex items-center justify-center order-last w-full px-2 py-3 transition-colors bg-transparent border-none rounded-none sm:py-2 text-inherit hover:bg-smoky-black-translucent hover:backdrop-blur-sm focus:bg-smoky-black-translucent hover:dark:bg-lotion-translucent focus:dark:bg-lotion-translucent sm:p-2 sm:rounded-full xl:py-2 xl:justify-start xl:rounded-md xl:gap-6"
    >
-    <ILoginLine className="w-7 h-7" />
+    <ILoginLine aria-labelledby="login icon" className="w-7 h-7" />
     <span className="hidden xl:inline-flex">Login</span>
    </Link>
   );
@@ -68,11 +80,10 @@ function UserRoutes({ activeProfile }: { activeProfile: boolean }) {
  return (
   <>
    {Auth}
-   {/* TODO: Change /account to /[username] when supported */}
-   {session?.user && (
+   {session?.user.username && (
     <Link
      title="Profile"
-     href="/profile"
+     href={`/${session.user.username}`}
      className={`${
       activeProfile ? "font-bold" : ""
      } order-last text-inherit flex w-full items-center justify-center border-none rounded-none px-2 py-3 sm:py-2 bg-transparent transition-colors hover:bg-smoky-black-translucent hover:backdrop-blur-sm focus:bg-smoky-black-translucent hover:dark:bg-lotion-translucent focus:dark:bg-lotion-translucent sm:px-2 sm:rounded-full xl:py-2 xl:justify-start xl:rounded-md xl:gap-6`}
