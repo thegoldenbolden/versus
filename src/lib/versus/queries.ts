f61b8021116da02bed9bf98c0526662f0bbe63c2
Jacob Bolden <jlbolden.pro@gmail.com>
2023-01-26 18:47:33 -0600

    Removed unused

--- src/lib/versus/queries.ts
+++
@@ -1,7 +0,0 @@
-export const versusKeys = {
- all: ["versus"] as const,
- lists: () => [...versusKeys.all, "list"] as const,
- list: (filters: any = {}) => [...versusKeys.lists(), { ...filters }] as const,
- details: () => [...versusKeys.all, "detail"] as const,
- detail: (id: string) => [...versusKeys.details(), id] as const,
-};
