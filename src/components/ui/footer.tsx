4ac0d61f3398a4877475a20f98d6e4c8d1bef179
Jacob Bolden <jlbolden.pro@gmail.com>
2023-01-26 18:28:03 -0600

    Added cookie policy

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -7,16 +7,32 @@ export default function Footer() {
    <AuthRoute className="opacity-75 hover:opacity-100 hover:underline focus:opacity-100 focus:underline" />
    <Link
     href="/privacy"
+    aria-label="privacy policy"
     className="opacity-75 hover:opacity-100 hover:underline focus:opacity-100 focus:underline"
    >
     Privacy Policy
    </Link>
    <Link
     href="/terms"
+    aria-label="terms of service"
     className="opacity-75 hover:opacity-100 hover:underline focus:opacity-100 focus:underline"
    >
     Terms of Service
    </Link>
+   <Link
+    href="/cookies"
+    aria-label="cookie policy"
+    className="opacity-75 hover:opacity-100 hover:underline focus:opacity-100 focus:underline"
+   >
+    Cookie Policy
+   </Link>
+   <a
+    aria-label="contact"
+    href="mailto:jlbolden.pro@gmail.com"
+    className="opacity-75 hover:opacity-100 hover:underline focus:opacity-100 focus:underline"
+   >
+    Contact
+   </a>
   </footer>
  );
 }
