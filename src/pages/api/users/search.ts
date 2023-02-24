7c2a4225a957508362572672afecd4fed7de0b5e
Jacob Bolden <jlbolden.pro@gmail.com>
2023-02-24 02:14:34 -0600

    use getUserByUsername function

--- src/pages/api/users/search.ts
+++ src/pages/api/users/search.ts
@@ -1,6 +1,7 @@
 import withApiHandler from "@lib/with-api-handler";
 import CustomError from "@lib/error";
 import prisma from "@lib/prisma";
+import getUserByUsername from "@lib/users/getUserByUsername";
 
 export default withApiHandler(async (req, versusId, sessionUserId) => {
  switch (req.method) {
@@ -8,31 +9,9 @@ export default withApiHandler(async (req, versusId, sessionUserId) => {
    throw new CustomError(405);
   case "GET":
    const { username } = req.query as { username?: string };
-
    if (!username) throw new CustomError(400);
-
-   const user = await prisma.user.findUniqueOrThrow({
-    where: { username },
-    select: {
-     name: true,
-     username: true,
-     image: true,
-     id: true,
-     followers: true,
-     following: true,
-     _count: { select: { followers: true, following: true } },
-    },
-   });
-
-   return {
-    data: {
-     name: user.name,
-     username: user.username,
-     image: user.image,
-     userFollows:
-      !sessionUserId || sessionUserId === user.id ? false : user.followers.length === 1,
-    },
-   };
+   const user = await getUserByUsername(username, sessionUserId);
+   return { user };
   case "DELETE":
    await prisma.user.delete({ where: { id: sessionUserId } });
    return;
